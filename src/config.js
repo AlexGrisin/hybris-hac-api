ff4ac9085857f19a1467596cfccc9d3dca2c5cc7
Alexandr Grisin <agrisin@tacitknowledge.com>
2022-03-31 18:38:25 +0300

    fix process env variable name

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 exports.config = {
   HAC_USER: process.env.USER_NAME || "admin",
   HAC_PASSWORD: process.env.PASSWORD || "nimda",
-  HAC_HOST: process.env.host,
+  HAC_HOST: process.env.HAC_HOST,
 };
